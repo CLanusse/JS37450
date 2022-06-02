77d1ef7a89b22e68f0b65b7791fd434b00af15ce
Conrado Lanusse <conrado.lanusse@gmail.com>
2022-06-01 21:20:27 -0300

    Clase 5 -1

---
+++ Clase 5 - Objetos/app.js
@@ -0,0 +1,55 @@
+// OBJETOS
+
+
+const usuario = {
+    nombre: "Conrado Lanusse",
+    edad: 34,
+    rol: "Profesor",
+    curso: "Javascript",
+    empresa: "Coderhouse"
+}
+
+
+// let obj1 = {
+//     nombre: "Jorge"
+// }
+
+// let obj2 = {
+//     nombre: "Jorge"
+// }
+
+// console.log(obj1 == obj2) // FALSE!
+
+
+// const usuario2 = {
+//     nombre: "Luis Salinas",
+//     edad: 26,
+//     rol: "Tutor",
+//     curso: "Javascript",
+//     empresa: "Coderhouse"
+// }
+
+// console.log(usuario.nombre)
+
+usuario.nombre = "Luis Salinas"
+usuario.edad += 1
+
+// console.log( usuario["email"] )
+
+// usuario["email"] = "abc@abc.com"
+
+// let propiedad = prompt("Qué propiedad acceder?") // "email"
+
+// usuario.propiedad = "abc@abc.com"
+
+// usuario[propiedad] = "abc@abc.com"
+
+
+// console.log(usuario)
+
+
+
+// delete usuario.email
+
+// -- CONSTRUCTORES --
+
