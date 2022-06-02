ff1ca569f8459ceba78fcf8aa6662cf777dcc81b
Conrado Lanusse <conrado.lanusse@gmail.com>
2022-06-01 22:33:48 -0300

    Clase objetos

--- Clase 5 - Objetos/app.js
+++ Clase 5 - Objetos/app.js
@@ -6,8 +6,17 @@ const usuario = {
     edad: 34,
     rol: "Profesor",
     curso: "Javascript",
-    empresa: "Coderhouse"
+    empresa: "Coderhouse",
+    activo: true,
+    direccion: {
+        calle: 'calle falsa',
+        num: 123,
+        barrio: null
+    }
 }
+
+// console.log(usuario.direccion.calle)
+// console.log(usuario.direccion.barrio)
 
 
 // let obj1 = {
@@ -54,5 +63,74 @@ usuario.edad += 1
 // -- CONSTRUCTORES --
 
 
+// function Mascota(nombre, edad, tipo) {
+//     this.nombre = nombre
+//     this.edad = edad
+//     this.tipo = tipo
 
-// UN CAMBIO NUEVO
+//     this.saludar = () => {
+//         console.log(this.nombre + " te saluda!")
+//     }
+
+//     this.cumplirAnios = () => {
+//         this.edad += 1
+//     }
+// }
+
+// const mascota1 = new Mascota("Ronnie", 6, "perro")
+// const mascota2 = new Mascota("Lucas", 3, "gato")
+// const mascota3 = new Mascota("Dory", 1, "pez")
+
+
+// mascota1.saludar()
+// console.log(mascota1)
+
+// mascota1.cumplirAnios()
+
+// console.log(mascota1)
+
+
+// console.log(mascota2)
+// console.log(mascota3)
+
+//  EJEMPLO APLICADO
+// function crearMascota() {
+//     let nombre = prompt('Ingrese nombre de la mascota')
+//     let edad = Number( prompt('Ingrese la edad de la mascota'))
+//     let tipo = prompt('Ingrese tipo de la mascota')
+
+//     const mascota = new Mascota(nombre, edad, tipo)
+//     return mascota
+// }
+
+// const mascota1 = crearMascota()
+// const mascota2 = crearMascota()
+
+// console.log(mascota1)
+// console.log(mascota2)
+
+
+// CLASES
+
+class Mascota {
+
+    constructor(nombre, edad, tipo) {
+        this.nombre = nombre
+        this.edad = edad
+        this.tipo = tipo
+    }
+
+    saludar() {
+        alert(this.nombre + " te está saludando!")
+    }
+
+    cumplirAnios() {
+        this.edad += 1
+    }
+}
+
+
+const mascota1 = new Mascota("Ronnie", 6, "perro")
+
+
+console.log(mascota1)
