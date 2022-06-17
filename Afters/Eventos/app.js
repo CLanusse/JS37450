711339f849fdefff99146c1afbad9b1aeb52f9c4
Conrado Lanusse <conrado.lanusse@gmail.com>
2022-06-16 22:56:13 -0300

    After eventos

---
+++ Afters/Eventos/app.js
@@ -0,0 +1,94 @@
+
+// == selectores ==
+
+const productosContainer = document.querySelector('#contenedor-productos')
+const carritoContenedor = document.querySelector('#carrito-contenedor')
+
+const contadorCarrito = document.querySelector('#contadorCarrito')
+const precioTotal = document.querySelector('#precioTotal')
+
+const btnVaciar = document.getElementById('vaciarCarrito')
+
+const carrito = []
+
+// generar el DOM de todos los productos
+stockProductos.forEach((producto) => {
+    const div = document.createElement('div')
+    div.classList.add('producto')
+
+    div.innerHTML = `
+                    <img src=${producto.img} alt="">
+                    <h3>${producto.nombre}</h3>
+                    <p>${producto.desc}</p>
+                    <p>Talle: ${producto.talle}</p>
+                    <p class="precioProducto">Precio: $${producto.precio}</p>
+                    <button onclick="agregarAlCarrito(${producto.id})" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>
+                `
+
+    productosContainer.append(div)
+})
+
+
+// function agregarAlCarrito() {
+
+// }
+
+const agregarAlCarrito = (id) => {
+    const item = stockProductos.find( (producto) => producto.id === id)
+    carrito.push(item)
+
+    console.log(carrito)
+    renderCarrito()
+    renderCantidad()
+    renderTotal()
+}
+
+const removerDelCarrito = (id) => {
+    const item = carrito.find((producto) => producto.id === id)
+    const indice = carrito.indexOf(item)
+    carrito.splice(indice, 1)
+
+    renderCarrito()
+    renderCantidad()
+    renderTotal()
+}
+
+const vaciarCarrito = () => {
+    carrito.length = 0
+
+    renderCarrito()
+    renderCantidad()
+    renderTotal()
+}
+
+btnVaciar.addEventListener('click', vaciarCarrito)
+
+const renderCarrito = () => {
+    carritoContenedor.innerHTML = ''
+
+    carrito.forEach((item) => {
+        const div = document.createElement('div')
+        div.classList.add('productoEnCarrito')
+
+        div.innerHTML = `
+                    <p>${item.nombre}</p>
+                    <p>Precio: $${item.precio}</p>
+                    <button onclick="removerDelCarrito(${item.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+                    `
+        
+        carritoContenedor.append(div)
+    })
+}
+
+const renderCantidad = () => {
+    contadorCarrito.innerText = carrito.length
+}
+
+const renderTotal = () => {
+    let total = 0
+    carrito.forEach((producto) => {
+        total += producto.precio
+    })
+
+    precioTotal.innerText = total
+}
