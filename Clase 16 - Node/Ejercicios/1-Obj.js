f049f1fba1f150903d6092e555c0bfc9ec51b963
Conrado Lanusse <conrado.lanusse@gmail.com>
2022-07-11 20:01:38 -0300

    Clase 16 - pre

---
+++ Clase 16 - Node/Ejercicios/1-Obj.js
@@ -0,0 +1,30 @@
+///1 - Encontrar la temperatura más baja de los tres días
+
+const temperaturasLunes = [16, 20, 5];
+const temperaturasMartes = [9, 15, 10];
+const temperaturasMiercoles = [14, 20, 8];
+
+
+// spread - min/max
+
+
+
+///2 - Agregar un atributo "peso" al objeto tomas
+
+let tomas = {
+  nombre: "Tomás",
+  especie: "Perro",
+  raza: "caniche",
+  edad: 14,
+};
+
+///3 - Crear una clase (o función constructora) que permita crear objetos tipo Mascota con
+// las mismas propiedades que tomás
+
+
+// 4 .- Usando métodos de array la clase anterior creada, agregar varios objetos de tipo Mascota
+// al array mascotas. Luego recorrer el array y buscar cuál es la mascota más joven, 
+// y luego cuál es la más pesada
+
+
+
